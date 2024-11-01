7b4f19e19977d1b8cf695cf06fdb030fc99a6539
Ghost_chu <ghostchu@111.com>
2024-11-01 19:21:08 +0800

    ZERO Host 处理

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,16 +2,16 @@ import inet.ipaddr.IPAddress;
 import inet.ipaddr.IPAddressString;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.StringJoiner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-    private static final int MERGE_TO_CIDR_AMOUNT_IPV4 = 5;
-    private static final int MERGE_TO_CIDR_AMOUNT_IPV6 = 5;
+    private static final int MERGE_TO_CIDR_AMOUNT_IPV4 = 3;
+    private static final int MERGE_TO_CIDR_AMOUNT_IPV6 = 3;
     private static final int IPV6_PREFIX_LENGTH = 56;
     private static final int IPV4_PREFIX_LENGTH = 24;
 
@@ -20,7 +20,20 @@ public class Main {
         if (!file.exists()) {
             throw new IllegalArgumentException("The file " + args[0] + " does not exist");
         }
-        Set<String> sorted = Files.readAllLines(file.toPath()).stream().sorted().collect(Collectors.toCollection(LinkedHashSet::new));
+        File target = new File(args[1]);
+        if (!target.exists()) {
+            throw new IllegalArgumentException("The file " + args[1] + " does not exist");
+        }
+        var files = file.listFiles((dir, name) -> name.endsWith(".txt"));
+        if(files == null){
+            throw new IllegalArgumentException("The file " + args[0] + " does not contain any text files");
+        }
+        List<String> allContent = new LinkedList<>();
+        for (File f : files) {
+            System.out.println("Reading: "+f.getName());
+            allContent.addAll(Files.readAllLines(f.toPath(), StandardCharsets.UTF_8));
+        }
+        List<String> sorted = allContent.stream().distinct().sorted().collect(Collectors.toList());
         Set<String> createdCIDR = new LinkedHashSet<>();
         int before = sorted.size();
         System.out.println("Before merging: " + before);
@@ -30,12 +43,11 @@ public class Main {
         for (String rule : sorted) {
             if (current == null) {
                 current = toCIDR(rule);
-                //System.out.println("Re-assigning current base to " + current);
                 continue;
             }
             var ip = toIP(rule);
             if (ip == null) {
-                System.out.println("(Unresolved IP) " + ip);
+                System.out.println("(Unresolved IP) " + rule);
                 continue;
             }
             if (ip.getPrefixLength() != null) {
@@ -80,7 +92,7 @@ public class Main {
         builder.add("# [END] Auto merged CIDR - IPV4: " + IPV4_PREFIX_LENGTH + ", IPV6: " + IPV6_PREFIX_LENGTH);
         sorted.forEach(builder::add);
         String data = builder.toString();
-        Files.writeString(file.toPath(), data);
+        Files.writeString(target.toPath(), data);
         System.out.println("CIDR count: " + createdCIDR.size());
         System.out.println("After merging: " + data.lines().count());
     }
@@ -96,9 +108,9 @@ public class Main {
         }
         ;
         if (ipAddress.isIPv4()) {
-            ipAddress = ipAddress.setPrefixLength(IPV4_PREFIX_LENGTH);
+            ipAddress = ipAddress.setPrefixLength(IPV4_PREFIX_LENGTH).toZeroHost();
         } else {
-            ipAddress = ipAddress.setPrefixLength(IPV6_PREFIX_LENGTH);
+            ipAddress = ipAddress.setPrefixLength(IPV6_PREFIX_LENGTH).toZeroHost();
         }
         return ipAddress.toZeroHost();
     }
